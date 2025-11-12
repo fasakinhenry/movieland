e0e4324eade1f0d51e661ff58f7b5938449b8bb8
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-12 13:48:25 +0100

    feat: successfully fetch mvoies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import './index.css'
+import { useEffect, useState } from 'react';
+import './index.css';
 
 const App = () => {
-  return (
-    <h1>Hello, React!</h1>
-  )
-}
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-export default App
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className='app'>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input
+          type='text'
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
